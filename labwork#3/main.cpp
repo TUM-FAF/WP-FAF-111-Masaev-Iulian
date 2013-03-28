e94debe357fb5b0b5d5b589af2b84b5146829998
leanu <polizovateli@mail.ru>
2013-03-28 22:23:12 +0200

    own func for bezier, still static

--- labwork#3/main.cpp
+++ labwork#3/main.cpp
@@ -1,5 +1,7 @@
 #include<windows.h>
 #include<math.h>
+#include<stdio.h>
+#define size 200
 
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
@@ -67,15 +69,41 @@ POINT find_near(POINT* apt1,POINT* apt2,POINT point)
     }
     return pt;
 }
+POINT* OwnBezier(POINT* pt,int width)
+{
+    POINT *bzr;
+    int i;
+    float t=0,mt,xcoord,ycoord;
+    POINT *pm;
+    pm=(POINT*)malloc(4*sizeof(POINT));
+    bzr=(POINT*)malloc(size*sizeof(POINT));
+    for(i=0;i<4;i++)
+    {
+        pm[i].x=pt[i].x+width/2; pm[i].y=pt[i].y;
+    }
+    bzr[0]=pm[0];
+    bzr[199]=pm[3];
+    for(i=1;i<200;i++)
+    {
+        t=t+0.005;
+        mt=1-t;
+        xcoord=mt*mt*mt*pm[0].x + 3*t*mt*mt*pm[1].x + 3*t*t*mt*pm[2].x + t*t*t*pm[3].x;
+        ycoord=mt*mt*mt*pm[0].y + 3*t*mt*mt*pm[1].y + 3*t*t*mt*pm[2].y + t*t*t*pm[3].y;
+        bzr[i].x=(int)xcoord;
+        bzr[i].y=(int)ycoord;
+    }
+    return bzr;
+}
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    HDC         hdc;
-    PAINTSTRUCT ps;
-    static POINT bzr[200],pt[4],bzg[15],point; //for own bezier, built in bezier, and beizergon with 5 points
-    static int          cxClient, cyClient;
-    RECT        rect;
-    HPEN        hwide,hvar,hbez;
+    HDC             hdc;
+    PAINTSTRUCT     ps;
+    static POINT    *bzr,pt[4],bzg[15],point; //for own bezier, built in bezier, and beizergon with 5 points
+    static int      cxClient, cyClient;
+    RECT            rect;
+    HPEN            hwide,hvar,hbez;
+    int i;
     switch (message)
     {
     case WM_CREATE:
@@ -92,7 +120,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
         pt[2].y = cyClient/4;
         pt[3].x = cxClient/3;
         pt[3].y = cyClient/8;
-
+        bzr=OwnBezier(pt,cxClient);
         return 0;
     case WM_PAINT:
         InvalidateRect (hwnd, NULL, TRUE);
@@ -105,7 +133,11 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
         LineTo(hdc,cxClient/2,cyClient/3); //draw a vertical line
         hbez=CreatePen(PS_SOLID,2,RGB(0,200,32));
         SelectObject(hdc,hbez);
+
         PolyBezier(hdc,pt,4); //draw bezier curve using implemented function in first rectangle
+        hbez=CreatePen(PS_SOLID,2,RGB(87,91,18));
+        SelectObject(hdc,hbez);
+        Polyline(hdc,bzr,size); //draw bezier curve using own function(for computing set of points)
         DeleteObject(hwide);
         DeleteObject(hbez);
         EndPaint(hwnd,&ps);
